ab5ca40515896956397a9bcfb3bd7b0705ae62f4
Angela Gomba <angelagomba@Angelas-MacBook-Pro.local>
2019-04-04 16:05:15 -0400

    scroll pls

--- Portfolios/scroll.js
+++
@@ -1,3 +0,0 @@
-$(window).scroll(function(){
-    $(".bio").css("opacity", 1 - $(window).scrollTop() / 250);
-});
--- js.js
+++ js.js
@@ -10,3 +10,15 @@ function stickyNavBar() {
         navbar.classList.remove("sticky");
     }
 }
+
+
+jQuery(function($) {
+    var divs = $('.bio');   //this selects the divs of class 'fade' (multiple)
+    $(window).on('scroll', function() {   //this selects the window and attaches to the scroll event.  when scroll occurs, the inline function is called
+        var st = $(this).scrollTop();   //gets the vertical position of the current element
+        divs.css({ //this changes the css of the divs from before (specifically margin-top and opacity
+            'margin-top' : -(st/3)+"px",
+            'opacity' : 1 - st/35
+        });
+    });
+});
